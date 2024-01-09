9f456d6603f39b393e3f5451388785552dfa5759
anirudhj2000 <anirudhjoshi985@gmail.com>
2024-01-09 16:29:32 +0530

    config changes

---
+++ metro.config.js
@@ -0,0 +1,42 @@
+const { getDefaultConfig } = require("expo/metro-config");
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts, assetExts },
+  } = await getDefaultConfig(__dirname);
+  return {
+    transformer: {
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: true,
+        },
+      }),
+    },
+    resolver: {
+      assetExts: [
+        ...assetExts,
+        "png",
+        "jpg",
+        "jpeg",
+        "gif",
+        "svg",
+        "ttf",
+        "otf",
+        "woff",
+        "woff2",
+        "obj",
+        "mtl",
+      ],
+      sourceExts: [
+        ...sourceExts,
+        "cjs",
+        "jsx",
+        "ts",
+        "tsx",
+        "mjs",
+        "md",
+        "mdx",
+      ],
+    },
+  };
+})();
